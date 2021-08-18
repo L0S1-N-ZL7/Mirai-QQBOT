5ed9ee5f68b31795c7a285b4be7a79534c5b5850
Violet-Sky <601982352@qq.com>
2021-08-19 00:11:07 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
+//自己的项目名称
+rootProject.name = "mirai-console-plugin-template"
