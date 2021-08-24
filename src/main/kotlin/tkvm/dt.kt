19ba178089a6fd83f643e01f12394ae8c229e28f
DELL <601982352@qq.com>
2021-08-24 15:51:19 +0800

    ok

--- src/main/kotlin/tkvm/dt.kt
+++ src/main/kotlin/tkvm/dt.kt
@@ -19,8 +19,6 @@ public class DATA{
     var traderPrice:String = "8"
 }
 
-
-
 fun main(args:Array<String>) {
 
 //    val client = OkHttpClient()
@@ -31,34 +29,59 @@ fun main(args:Array<String>) {
 //    json.put("B","2")
 //    json.put("C","3")
 
-    //var IN = ""
-    val url:String = "https://tarkov-market.com/api/v1/item?q=BTC&x-api-key=LtqsedrEFLY3vGE5"
+    var IN = "BTC"
+    val api = "https://tarkov-market.com/api/v1/item?q=$IN&x-api-key=LtqsedrEFLY3vGE5"
 
-    val builder = FormBody.Builder()
-    //builder.add("",  "BTC")
-    val formBody = builder.build()
-
-    val request = Request.Builder()
-        .method("POST", formBody)
-        .url(url).build()
-
-    val client = OkHttpClient()
-
-    client.newCall(request).enqueue(object : Callback {
-
-        override fun onResponse(call: Call, response: Response) {
-            val result = response.body?.string()
-            println("result：$result")
-        }
-
+    val url = api
+    val okHttpClient = OkHttpClient()
+    val request: Request = Request.Builder()
+        .url(url)
+        .get() //默认就是GET请求，可以不写
+        .build()
+    val call = okHttpClient.newCall(request)
+    println(call)
+    println("\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n")
+    call.enqueue(object : Callback {
         override fun onFailure(call: Call, e: IOException) {
-            println("Failed request api :( " + e.message)
+            println("onFailure: ")
         }
 
+        @Throws(IOException::class)
+        override fun onResponse(call: Call, response: Response) {
+            println("onResponse: " + response.body!!.string())
+        }
     })
 
+
+
+//    //var IN = ""
+//    val url:String = "https://tarkov-market.com/api/v1/item?q=BTC&x-api-key=LtqsedrEFLY3vGE5"
+//
+//    val builder = FormBody.Builder()
+//    //builder.add("",  "BTC")
+//    val formBody = builder.build()
+//
+//    val request = Request.Builder()
+//        .method("POST", formBody)
+//        .url(url).build()
+//
+//    val client = OkHttpClient()
+//
+//    client.newCall(request).enqueue(object : Callback {
+//
+//        override fun onResponse(call: Call, response: Response) {
+//            val result = response.body?.string()
+//            println("result：$result")
+//        }
+//
+//        override fun onFailure(call: Call, e: IOException) {
+//            println("Failed request api :( " + e.message)
+//        }
+//
+//    })
+
 //    val tkv = DATA()
-//    println()
+//
 //    println(tkv.name)
 //    println(tkv.shortName)
 //    println(tkv.price)
