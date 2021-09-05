6f94fc6d8638ceacea4f4b6f3d7712568acb8ebb
Violet-Sky <601982352@qq.com>
2021-09-05 23:55:24 +0800

    改了什么 好像没改什么

--- src/main/kotlin/GroupMessageMonitoring.kt
+++ src/main/kotlin/GroupMessageMonitoring.kt
@@ -2,7 +2,9 @@ package org.example.mirai.plugin
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.message.data.MessageSource.Key.quote
+import net.mamoe.mirai.message.data.MessageSource.Key.recall
 import net.mamoe.mirai.message.data.MessageSource.Key.recallIn
+import net.mamoe.mirai.message.data.source
 
 class GroupMessageMonitoring(gme:GroupMessageEvent) {
     var gme = gme
@@ -11,7 +13,7 @@ class GroupMessageMonitoring(gme:GroupMessageEvent) {
         if(gme.message.contentToString() == "跳蚤#BTC"){
             gme.group.sendMessage(
                 gme.message.quote() +
-                    At(gme.sender) +
+                    At(gme.sender) + "\n" +
                     "物品名：" + tkvdt.name + "\n" +
                     "当前价格：" + tkvdt.price + "\n" +
                     "24h均价：" + tkvdt.avg24hPrice + "\n" +
@@ -20,17 +22,31 @@ class GroupMessageMonitoring(gme:GroupMessageEvent) {
                     "商人收购价：" + tkvdt.traderPrice + "\n" +
                     "玩你🐴的垃圾游戏呢😅"
             )
-            gme.message.recallIn(1)
             return
         }
 
-        if (gme.message.contentToString() == "你妈的") {
+        if (gme.message.contentToString() == "前source") {
+            ////因为message已经被实例化了 所以可以使用message去调用撤回的方法 recallin（1millis 1分钟）
+            gme.message.source.recallIn(5000)
+            gme.message.recallIn(5000)
+            //群内发送
+            gme.group.sendMessage(
+                At(gme.sender) //At((消息.)发送者)
+                    + gme.message.quote() + "Play your 🐴🉐️🌶️🐔🎮呢🧍‍♂️😅")
+            gme.message.source.recall()
+            gme.message.recall()
+            //向发送者私聊发送消息
+            //不继续处理
+            return
+        }
+        if (gme.message.contentToString() == "后source") {
             //群内发送
             gme.group.sendMessage(
                 At(gme.sender) //At((消息.)发送者)
                     + gme.message.quote() + "Play your 🐴🉐️🌶️🐔🎮呢🧍‍♂️😅")
             ////因为message已经被实例化了 所以可以使用message去调用撤回的方法 recallin（1millis 1分钟）
-            gme.message.recallIn(1)
+            gme.message.source.recallIn(5000)
+            gme.message.recallIn(5000)
             //向发送者私聊发送消息
             //不继续处理
             return
