f8d6a5cc93e487cbf889e8d62d1fbd1ca478112b
Sky-mac <601982352@qq.com>
2021-09-05 16:09:08 +0800

    BETA

---
+++ src/main/kotlin/GroupMessageMonitoring.kt
@@ -0,0 +1,7 @@
+package org.example.mirai.plugin
+import net.mamoe.mirai.event.events.GroupMessageEvent
+
+class GroupMessageMonitoring(gme:GroupMessageEvent) {
+    var gme = gme
+    fun open(){}
+}
